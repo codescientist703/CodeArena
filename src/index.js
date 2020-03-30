30aa48bbbc0ab011a672706c19e894fa8d7e48a7
codescientist703 <pukimonlegend@gmail.com>
2020-03-30 13:37:08 +0530

    fixed the path

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App'
+import { BrowserRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
-  <App />,
+ <Router basename={process.env.PUBLIC_URL}><App /></Router>,
   document.getElementById('root')
 );
 
